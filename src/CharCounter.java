60e3f3c6f4e6c917351c72817dbbedaa5a08c58d
tejb96 <104933640+tejb96@users.noreply.github.com>
2023-07-04 20:47:41 -0600

    file reader

--- src/CharCounter.java
+++ src/CharCounter.java
@@ -1,4 +1,23 @@
-
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.*;
 public class CharCounter {
-
+	private int[] charCounts = new int[65536];
+	private String line, word;
+	
+	public CharCounter(String file) throws FileNotFoundException {
+		File text = new File(file);
+		Scanner reader =new Scanner(text);
+		StringTokenizer token;
+		
+		while(reader.hasNext()) {
+			line = reader.nextLine();
+			token = new StringTokenizer(line);
+			while(token.hasMoreTokens()) {
+				word=token.nextToken();				
+				System.out.println(word);
+				
+			}
+		}
+	}
 }
--- src/SearchCLI.java
+++ src/SearchCLI.java
@@ -4,7 +4,7 @@ import java.io.IOException;
  * @author Tej
  */
 public class SearchCLI {
-	
+	private CharCounter count;
 	private InteractiveCLI cli;
 	
 	public SearchCLI() {
@@ -14,10 +14,18 @@ public class SearchCLI {
 	public void run() {		
 		cli.prompt("Enter input file name: ");
 		String filename = cli.getKeyboardString();
+		
+		try {
+			count = new CharCounter(filename);
+		}catch(IOException e) {
+			cli.display(e.getMessage() + " - please try again.");
+			return;
+		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SearchCLI app = new SearchCLI();
+		app.run();
 	}
 
 }
