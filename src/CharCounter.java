bc2d650343a684a9f83b38694b9c502b8c684337
tejb96 <104933640+tejb96@users.noreply.github.com>
2023-07-06 13:31:37 -0600

    print count

--- src/CharCounter.java
+++ src/CharCounter.java
@@ -3,20 +3,28 @@ import java.util.StringTokenizer;
 import java.io.*;
 public class CharCounter {
 	private int[] charCounts = new int[65536];
-	private String line, word;
+	private String line;
 	
 	public CharCounter(String file) throws FileNotFoundException {
 		File text = new File(file);
 		Scanner reader =new Scanner(text);
-		StringTokenizer token;
 		
 		while(reader.hasNext()) {
 			line = reader.nextLine();
-			token = new StringTokenizer(line);
-			while(token.hasMoreTokens()) {
-				word=token.nextToken();				
-				System.out.println(word);
-				
+			char[] arr=line.toCharArray();
+			for(int i=0;i<arr.length;i++) {
+				int character = arr[i];
+				charCounts[character]+=1;				
+			}	
+		}
+		
+	}
+	
+	public void printCount() {
+		for(int i=0;i<65536;i++) {
+			if(charCounts[i]>0) {
+				char c=(char) i;
+				System.out.println(c+" - "+charCounts[i]);
 			}
 		}
 	}
